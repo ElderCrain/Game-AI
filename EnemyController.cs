8a21fe51486bc37b63250ca2f42b97650b7e916c
ElderCrain <43729332+ElderCrain@users.noreply.github.com>
2018-09-30 14:24:51 -0400

    Add files via upload

---
+++ EnemyController.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    
+    public float timeToSpotPlayer = .5f;
+
+    Transform target;
+    NavMeshAgent agent;
+    public Transform home;
+    public float viewDistance;
+    public float maxviewDistance;
+    public LayerMask viewMask;
+    float viewAngle;
+    public float maxviewAngle;
+    float playerVisibleTimer;
+    Transform player;
+    Color originalSpotlightColor2;
+    public Light spotlight2;
+    
+    
+
+
+    // Use this for initialization
+    void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+        target = playerManager.instance.player.transform;
+        
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+
+        viewAngle = spotlight2.spotAngle;
+
+        originalSpotlightColor2 = spotlight2.color;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+        
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= 4f)
+        {
+            FaceTarget();
+        }
+
+        if (CanSeePlayer())
+        {
+            playerVisibleTimer += Time.deltaTime;
+            viewAngle += Time.deltaTime;
+            viewDistance += Time.deltaTime;
+            spotlight2.spotAngle = 120f;
+
+        }
+        else
+        {
+            playerVisibleTimer -= Time.deltaTime;
+            viewAngle -= Time.deltaTime;
+            viewDistance -= Time.deltaTime;
+            spotlight2.spotAngle = 80f;
+
+        }
+        playerVisibleTimer = Mathf.Clamp(playerVisibleTimer, 0, timeToSpotPlayer);
+        viewAngle = Mathf.Clamp(viewAngle, 80, maxviewAngle);
+        viewDistance = Mathf.Clamp(viewDistance, 10, maxviewDistance);
+        spotlight2.color = Color.Lerp(originalSpotlightColor2, Color.red, playerVisibleTimer / timeToSpotPlayer);
+
+        if (playerVisibleTimer >= timeToSpotPlayer)
+        {
+            StartCoroutine(Movetopoint());
+            
+
+        }
+
+        
+
+
+    }
+
+    bool CanSeePlayer()
+    {
+        if (Vector3.Distance(transform.position, player.position) < viewDistance)
+            
+        {
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            float angleBetweenGuardAndPlayer = Vector3.Angle(transform.forward, dirToPlayer);
+            if (angleBetweenGuardAndPlayer < viewAngle / 2f)
+            {
+                if (!Physics.Linecast(transform.position, player.position, viewMask))
+                {
+                    
+                    
+                    return true;
+
+                }
+            }
+
+        }
+        
+        return false;
+    }
+
+    private bool AtPoint()
+    {
+
+        float dist = agent.remainingDistance;
+        if (dist != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+        {
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+
+    IEnumerator Movetopoint()
+    {
+        
+        agent.destination = player.position;
+        yield return new WaitUntil(AtPoint);
+        yield return new WaitForSecondsRealtime(1);
+        agent.destination = home.position;
+        yield return new WaitUntil(AtPoint);
+        
+
+
+
+    }
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime *5f);
+
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
+
+    }
+
+    
+
+    
+}
